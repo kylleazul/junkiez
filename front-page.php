cbe81e2eeb062e725e95a88406b5147869faa91f
janedmonddevera <edmonddevera000@gmail.com>
2024-10-31 13:22:07 +0800

    second commit

--- front-page.php
+++ front-page.php
@@ -1,110 +1,172 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Full-Screen Navbar</title>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7e8116745f.js" crossorigin="anonymous"></script>
     <style>
         body {
             height: 200vh; /* Create enough scrollable content */
             margin: 0;
+            background-color: #f2f2f2;
         }
 
         .navbar {
-            transition: transform 0.3s ease; /* Smooth transition */
-        }
-
-        .navbar-fullscreen {
             position: fixed;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(255, 255, 255, 0.95); /* Slightly transparent background */
-            z-index: 999; /* Ensure it overlays other content */
-            transform: translateY(0); /* Reset transform */
+            right: 0;
+            background: #333;
+            color: #f2f2f2;
+            padding: 10px 20px;
+            transition: top 0.3s;
+           background-color: #f2f2f2;
+        }
+        .hidden {
+            opacity: 0;
+        }
+     
+
+        .sidenav {
+  height: 100%;
+  width: 0; /* Start hidden */
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0; /* Align to the right */
+  background-color: #f2f2f2;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav h1:hover {
+  color: black;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+
+.sidenav .title {
+  position: absolute;
+  top: 0;
+  left: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+  text-transform: uppercase;
+  color: black;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding: 16px;
+}
+
+.header {
+            display: flex;
+            justify-content: space-between; /* Aligns items to the edges */
+            align-items: center; /* Centers items vertically */
         }
 
-        .navbar.hidden {
-            transform: translateY(-100%); /* Hide navbar */
-        }
 
-        .dropdown {
-            position: relative; /* Ensure dropdown is positioned correctly */
-        }
-
-        .dropdown-menu {
-            height: 100%; /* Make dropdown take full height */
-            top: 0; /* Align the dropdown to the top */
-            margin: 0; /* Remove margin */
-        }
     </style>
-</head>
-<body>
 
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+
+<nav class="navbar navbar-light ">
+    <a class="navbar-brand hidden" href="#">JunkieZ</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon align-items-end justify-content-end" onclick="openNav()"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu dropdown-menu-end text-right h-100" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+    <div id="mySidenav" class="sidenav ">
+   
+    <a class="title" href="#">JunkieZ</a>
+  
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <div class="p-4 m-4">
+  <a href="/wp-content/themes/junkiez/intro.php"><h1 class="fs-1">Introduction</h1></a>
+  <a href="#"><h1>Mision Vision Advocacy</h1></a>
+  <a href="#"><h1>Infographics</h1></a>
+  <a href="#"><h1>Articles</h1></a>
+  <a href="#"><h1>Articles</h1></a>
+ 
+
+  <div class="footer d-flex justify-content-between align-items-start p-4 m-4">
+    <div class="align-items-start">
+    <i class="fa-brands fa-facebook fa-2xl m-2" style="color: #000000;"></i>
+  <i class="fa-brands fa-instagram fa-2xl m-2" style="color: #000000;"></i>
+  <i class="fa-regular fa-envelope fa-2xl m-2" style="color: #000000;"></i>
     </div>
+    <div class="align-contents-end">
+          <p class="text-dark">
+          JunkieZ <i class="fa-regular fa-copyright" style="color: #000000;"></i> 2024. All Rights Reserved
+          </p>  
+          <a href="#">Terms and Condition</a>
+    </div>
+ 
+  </div>
+  </div>
+  </div>
+</div>
 </nav>
 
-<div class="container-fluid full-height d-flex justify-content-center align-items-center mt-2">
-    <div class="text-center mt-4">
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-        <h1 class="mt-4">Centered Div</h1>
-    </div>
-</div>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script>
+        let lastScrollTop = 0;
+        const navbar = document.querySelector('.navbar-brand');
+        const navbar1 = document.querySelector('.navbar');
+        window.addEventListener('scroll', function() {
+            const st = window.pageYOffset || document.documentElement.scrollTop;
+            if (st > lastScrollTop && st > 30) {
+                // Downscroll and past 30px
+                navbar.classList.remove('hidden'); // Hide navbar
+
+                navbar1.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+            } 
+           else if ( st === 0) {
+          
+            navbar.classList.add('hidden'); 
+           navbar1.style.backgroundColor = '#f2f2f2';
+           }
+        });
+    </script>
 
 <script>
-    const navbarToggle = document.querySelector('.navbar-toggler');
-    const navbar = document.querySelector('.navbar');
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100vw";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-    navbarToggle.addEventListener('click', function() {
-        navbar.classList.toggle('navbar-fullscreen'); // Toggle full-screen class
-    });
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+
+
+  
+}
+
+
+
+
 </script>
-</body>
-</html>
+
