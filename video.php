dea6496bb208133bb42fe1f44b9ff6be783d9cba
kylleazul <kylle.azul@d-source.com.ph>
2024-11-05 13:49:27 +0800

    push

---
+++ video.php
@@ -0,0 +1,21 @@
+<?php
+
+     /*
+
+Template Name: Video Page Template
+
+*/
+
+
+        get_header();
+?>
+
+<div class="iconContainer">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/junkiez.png" alt="" class="imgLogo">
+</div>
+
+<?php
+
+        get_footer();
+
+?>
