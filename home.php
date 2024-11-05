f5ec598ddcd0ae32591c5649c65b785c3d9fe8f7
kylleazul <kylle.azul@d-source.com.ph>
2024-11-05 16:51:23 +0800

    push

--- home.php
+++ home.php
@@ -11,7 +11,7 @@ Template Name: Home Template
 ?>
 
 <div class="iconContainer">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/junkiez.png" alt="">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/junkiez.png" alt="" class="imgLogo">
 </div>
 
 <div class="mainContainer">
@@ -33,23 +33,40 @@ Template Name: Home Template
 
 <div class="wholeImgContainer">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/DSC09663.JPG" alt="" class="imgBody3">
-    <h1 class="floatingText">BE INFORMED!<h1>
+    <h1 class="floatingText">!!! BE INFORMED !!!<h1>
 </div>
 
-<div class="threeColumnImgContainer">
+<!-- <div class="threeColumnImgContainer">
     <div class="firstSection">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cropped_DSC09909.JPG" alt="" class="imgBody4">
         <h3>What's happening around you?</h3>
     </div>
     <div class="secondSection">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cropped_DSC09719.JPG" alt="" class="imgBody4">
         <h3>Know your trash</h3>
     </div>
     <div class="thirdSection">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/DSC09499.JPG" alt="" class="imgBody4">
         <h3>Become Trash-worthy</h3>
     </div>
+</div> -->
+
+<div class="scroll-container">
+    <ul>
+        <li>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cropped_DSC09909.JPG" alt="" class="imgBody4">
+            <h3>What's happening around you?</h3>
+        </li>
+        <li>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cropped_DSC09719.JPG" alt="" class="imgBody5">  
+            <h3>Know your trash</h3>
+        </li>
+        <li>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/DSC09499.JPG" alt="" class="imgBody6">
+            <h3>Become Trash-worthy</h3>
+        </li>
+    </ul>
 </div>
+
+
+
 
 <?php
 
