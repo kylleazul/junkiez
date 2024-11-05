3e655ba9aef9203c8e00d54760f64301b9b03c5c
kylleazul <kylle.azul@d-source.com.ph>
2024-11-05 14:07:27 +0800

    push to kylle branch

--- video.php
+++ video.php
@@ -14,6 +14,10 @@ Template Name: Video Page Template
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/junkiez.png" alt="" class="imgLogo">
 </div>
 
+<div class="videoLink">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/yj1VNtODFG4?si=W9xkli9pbe19Qylf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
 <?php
 
         get_footer();
